a2ba0cdd3929296c5dc3d3b8dc7d9a0f4ff5e13e
Randy Schmidt <randy@forge38.com>
2011-04-18 16:56:02 -0400

    Create a post from postmark

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,13 @@
+class Api::PostsController < ApplicationController
+  def create
+    # make sure the thing posting has rights to post here... maybe with
+    # http basic auth or a super secret token
+
+    Post.create_from_postmark(Postmark::Mitt.new(request.body.read))
+
+    # do a bunch of stuff like send an email back if the user can't be
+    # found, doesn't exist, etc.
+
+    render :text => "Created a post!", :status => :created
+  end
+end
