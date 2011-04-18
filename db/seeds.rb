46687dee524abff5673dbf90df83df1a3d5d190a
Randy Schmidt <randy@forge38.com>
2011-04-18 16:35:16 -0400

    Add another user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 User.create!(:name => 'Tyler Durden', :api_token => 'tyler') unless User.exists?(:api_token => 'tyler')
+User.create!(:name => 'Robert Paulson', :api_token => 'robert') unless User.exists?(:api_token => 'robert')
