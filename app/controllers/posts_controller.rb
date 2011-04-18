96ac1f6cf3fce6113b5a35656b9bb53061577d1f
Randy Schmidt <randy@forge38.com>
2011-04-18 16:42:22 -0400

    Post index page

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.limit(10)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 PostmarkMittExample::Application.routes.draw do
+  namespace :api do
+    resources :posts, :only => [ :create ]
+  end
+  root :to => 'posts#index'
 end
